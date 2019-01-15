c6f892c6aeb81f9fce7661064f231044ffeba8cb
alexandra <le.onze.mai@gmail.com>
2019-01-15 22:13:59 +0300

    made form js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+  var openPopupButtonsCollection = document.querySelectorAll('.js-open-popup');
+  var popupCloseSelector = document.querySelector('.js-popup-close');
+  var popup = document.querySelector('.js-popup');
+  var openPopup = function() {
+    popup.classList.add('popup--show');
+  };
+  var closePopup = function() {
+    popup.classList.remove('popup--show');
+  };
+
+  openPopupButtonsCollection.forEach(function(button) {
+    button.addEventListener('click', openPopup);
+  });
+
+  popupCloseSelector.addEventListener('click', closePopup);
+};
